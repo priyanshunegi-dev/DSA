a9529e73aec1d633aa3522deff848ede07c7a122
priyanshunegi-dev <priyanshu.negi@virtuaresearch.com>
2026-03-22 17:33:37 +0530

    Update SlidingWindow_MinSizeSubarraySum.java

--- SlidingWindow_MinSizeSubarraySum.java
+++ SlidingWindow_MinSizeSubarraySum.java
@@ -28,14 +28,22 @@ Space Complexity: O(1)
 
 class SlidingWindow_MinSizeSubarraySum {
 
-    public int minSubArrayLen(int target, int[] nums) {
-        // TODO: Implement sliding window logic here
-        
-        
-        
-        
-        
-        return 0; // replace with actual result
+   public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+    
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+    
+            while (sum >= target) {
+                min = Math.min(min, i - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+    
+        return min == Integer.MAX_VALUE ? 0 : min;
     }
 
     public static void main(String[] args) {
